ef3e78f3fb231c837dae4420ad2e92df39108fa1
Ahsan2519 <ansariahsan67@gmail.com>
2023-09-10 01:32:20 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "../assets/App.css";
+
+const App = ()=> {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <dir>hi</dir>
+    </>
+  );
+}
+
+export default App;
